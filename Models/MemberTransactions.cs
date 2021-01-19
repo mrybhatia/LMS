1f24efc272cc48052844bef2838d83f9df1de47a
mrybhatia <36405508+mrybhatia@users.noreply.github.com>
2021-01-19 15:09:41 +0530

    added models

---
+++ Models/MemberTransactions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS.Models
+{
+    public class MemberTransactions
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int BookId { get; set; }
+
+        public int MemberId { get; set; }
+
+        public DateTime IssueDate { get; set; }
+
+        public DateTime ReturnDate { get; set; }
+    }
+}
