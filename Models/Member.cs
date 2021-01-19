02040d74fbbf6d59978b52e0ed1fe3ab0c9412b0
mrybhatia <36405508+mrybhatia@users.noreply.github.com>
2021-01-19 15:09:02 +0530

    created new folder

---
+++ Models/Member.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ContactNumber { get; set; }
+    }
+}
