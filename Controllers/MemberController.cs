4e0f9b71d6fe389f53c518b3838123b46bfb4b41
mrybhatia <36405508+mrybhatia@users.noreply.github.com>
2021-01-19 15:11:57 +0530

    Added Member controller

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LMS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.Controllers
+{
+    [Route("api/member")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        [Route("{memberId}")]
+        public Member GetMember(int memberId)
+        {
+            return new Member();
+        }
+
+        [Route("all")]
+        public IEnumerable<Member> GetAllMembers()
+        {
+            return new List<Member>();
+        }
+
+        [Route("add")]
+        public bool PostAddMember(Member member)
+        {
+            return true;
+        }
+
+        [Route("delete")]
+        public bool DeleteMember(int memberId)
+        {
+            return true;
+        }
+
+        [Route("update")]
+        public bool PutUpdateMember(Member member)
+        {
+            return true;
+        }
+    }
+}
